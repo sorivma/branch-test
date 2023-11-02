287183d5a2c27ed1b09d0d472e20cd0f2cb25f4b
soriv <sorivma03.06@gmail.com>
2023-11-02 17:30:34 +0300

    add Main.mainDude at main

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке main");
+        mainDude();
+    }
+
+    private static void mainDude() {
+        System.out.println("ama main void dude");
     }
 }

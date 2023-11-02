edd2f64fb4b1ac6971cd26492adcaae48bec9b8f
soriv <sorivma03.06@gmail.com>
2023-11-02 17:20:55 +0300

    add Main.doStuff in dev

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Это написано в dev");
+        doStuff();
     }
+
+    public static void doStuff() {
+        System.out.println("doin stuff");
+    }
+
 }
